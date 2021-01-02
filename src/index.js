a35253dc3f89a4e31bef1ed00cb12708cd716300
Carlos <carlos.escobar_uk@live.com>
2021-01-02 10:51:58 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// Redux
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
